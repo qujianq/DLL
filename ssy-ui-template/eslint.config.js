f2bc24e00c1aee77e16aa4b933a559af0d4a81fa
qujianq <1750970493@qq.com>
2024-11-05 13:33:47 +0800

    jjct2

---
+++ ssy-ui-template/eslint.config.js
@@ -0,0 +1,23 @@
+import pluginVitest from '@vitest/eslint-plugin'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
+]
